b35c50701f4102baf3026dd4ff853f268b148536
Amanda Bui <buixx199@umn.edu>
2021-04-18 14:43:38 -0500

    Spellcheck false for name fields

--- js/clericAssistJs.js
+++ js/clericAssistJs.js
@@ -7,7 +7,7 @@ var submitCharButton = document.getElementById("submit-char-button");
 // GLOBALS
 var characterFields = ["name-field", "hp-field", "ac-field"];
 var addCharButtonClicked = false;
-var editValues = {};
+var editValues = [];
 
 // FUNCTIONS FOR ADDING
 function addNewCharacter() {
@@ -35,13 +35,6 @@ function submitNewCharacter() {
     var tbl = document.getElementById("player-table");
     var newTextRow = tbl.insertRow(tbl.rows.length);
     characterFields.forEach(id => insertData(newTextRow, document.getElementById(id)));
-
-    var newEditRow = tbl.insertRow(tbl.rows.length);
-    characterFields.forEach(id => insertEditCells(newEditRow, document.getElementById(id)));
-    newEditRow.setAttribute('style', 'display : none');
-
-    createButton(newTextRow, newEditRow, "Edit");
-    createButton(newEditRow, newTextRow, "Save");
     clearFields();
   } else {
     alert("ENTER IN VALUES K THX");
@@ -51,36 +44,20 @@ function submitNewCharacter() {
 function insertData(row, data) {
   var td = document.createElement("td");
   td = row.insertCell(-1);
-  var textNode = document.createTextNode(data.value);
-  td.append(textNode);
-}
-
-function insertEditCells(row, data, id) {
-  var td = document.createElement('td');
-  td = row.insertCell(-1);
-  var editNode = document.createElement('INPUT');
-
-  if (data.type == 'text') {
-    editNode.setAttribute("type", "text");
-  } else if (data.type == 'number') {
-    editNode.setAttribute("type", "number");
-  }
-  editNode.value = data.value;
-  td.append(editNode);
-}
-
-function createButton(row, otherRow, text) {
-  var td = document.createElement("td");
-  td = row.insertCell(-1);
-  var button = document.createElement('BUTTON');
-  button.innerHTML = text;
-  button.onclick = function() {
-    row.setAttribute("style", "display : none");
-    otherRow.setAttribute("style", "display : contents");
-    if (text == 'Save') {
+  td.contentEditable = true;
+  td.addEventListener("keypress", function(event) {
+    if (data.type == 'number') {
+      // prevent users from entering in chars in HP and AC field
+      if (event.charCode < 48 || event.charCode > 57) {
+        event.preventDefault();
+      }
     }
+  });
+  var textNode = document.createTextNode(data.value);
+  if (data.type == 'text') {
+    td.spellcheck = false;
   }
-  td.append(button);
+  td.append(textNode);
 }
 
 function clearFields() {
@@ -88,12 +65,6 @@ function clearFields() {
   addCharButton.value = "+";
   addCharButtonClicked = false;
   characterFields.forEach(id => document.getElementById(id).value = "");
-}
-
-// FUNCTIONS FOR EDITING
-function editCharacter() {
-  // convert all the fields into textfields with
-  // on edit set contenteditable for the associated fields
 }
 
 window.addEventListener("keydown", function (event) {
